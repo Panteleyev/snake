e3f6488d755722c4bb28b018cd893f1cbb8fc527
Panteleyev <gleb.panteleyev@mail.ru>
2016-05-03 00:49:01 +0400

    Осваиваем класс "Список". Эксперименты с классом List

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -8,13 +8,49 @@ namespace Snake
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main( string[] args )
         {
             Point p1 = new Point( 1, 3, '*' );
             p1.Draw();
 
             Point p2 = new Point( 4, 5, '#' );
             p2.Draw();
+
+            Point p3 = new Point( 2, 5, '@' );
+            p3.Draw();
+
+            Point p4 = new Point( 7, 2, '+' );
+            p4.Draw();
+
+            List<int> numList = new List<int>();
+            numList.Add( 0 );
+            numList.Add( 4 );
+            numList.Add( 5 );
+            numList.Add( 6 );
+
+            int x = numList[ 0 ];
+            int y = numList[ 1 ];
+            int z = numList[ 2 ];
+
+            foreach (int i in numList)
+            {
+                Console.WriteLine( i );
+            }
+
+            numList.RemoveAt( 1 );
+
+            List<Point> pList = new List<Point>();
+            pList.Add( p1 );
+            pList.Add( p2 );
+            pList.Add( p3 );
+            pList.Add( p4 );
+            pList.Add( new Point( 6, 4, '-' ) );
+            pList[ 4 ].Draw();
+
+            foreach (Point i in pList)
+            {
+                Console.WriteLine( "[" + i.x + ":" + i.y + "]: " + i.sym );
+            }
 
             Console.ReadLine();
         }
