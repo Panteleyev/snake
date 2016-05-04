873463d4a380acf2596984a0980dc52d48497c57
Panteleyev <gleb.panteleyev@mail.ru>
2016-05-04 14:08:24 +0400

    Класс "Препятствия". Полиморфизм

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -11,59 +11,37 @@ namespace Snake
     {
         static void Main( string[] args )
         {
-            int frameWidth = 80,
-                frameHeight = 25;
-
-            Console.WriteLine( "Нажмите enter, чтобы начать" );
-            Console.ReadLine();
-
-            // Отрисовка рамочки
-            Console.SetBufferSize( frameWidth, frameHeight );
-            Console.SetWindowSize( frameWidth, frameHeight );
-            Console.CursorVisible = false;
-
-            HorizontalLine upLine = new HorizontalLine( 0, frameWidth - 2, 0, '+' );
-            HorizontalLine downLine = new HorizontalLine( 0, frameWidth - 2, frameHeight - 1, '+' );
-            VerticalLine leftLine = new VerticalLine( 0, frameHeight - 1, 0, '+' );
-            VerticalLine rightLine = new VerticalLine( 0, frameHeight - 1, frameWidth - 2, '+' );
-            upLine.Draw();
-            downLine.Draw();
-            leftLine.Draw();
-            rightLine.Draw();
+            VerticalLine v1 = new VerticalLine( 0, 10, 5, '%' );
+            Draw( v1 );
 
             // Отрисовка точек
             Point p = new Point( 4, 5, '*' );
             //p.Draw();
-            Snake snake = new Snake( p, 4, Direction.RIGHT );
-            snake.Draw();
 
-            // Создаем еду
-            FoodCreator foodCreator = new FoodCreator( frameWidth, frameHeight, '$' );
-            Point food = foodCreator.CreateFood();
-            food.Draw();
+            //Snake snake = new Snake( p, 4, Direction.RIGHT );
+            Figure fSnake = new Snake( p, 4, Direction.RIGHT );
+            Draw( fSnake );
+            Snake snake = (Snake)fSnake; // Приведение типа явно
+            //snake.Move...
 
-            while (true)
+            HorizontalLine h1 = new HorizontalLine( 0, 5, 6, '&' );
+
+            List<Figure> figures = new List<Figure>();
+            figures.Add( fSnake );
+            figures.Add( v1 );
+            figures.Add( h1 );
+
+            foreach (var f in figures)
             {
-                if (snake.eat( food ))
-                {
-                    food = foodCreator.CreateFood();
-                    food.Draw();
-                }
-                else
-                {
-                    snake.Move( frameWidth, frameHeight );
-                }
-                Thread.Sleep( 100 );
-
-                if (Console.KeyAvailable)
-                {
-                    ConsoleKeyInfo key = Console.ReadKey( true );
-                    while (Console.KeyAvailable) key = Console.ReadKey( true ); // во избежании очереди за новым направлением
-                    snake.HandleKey( key.Key );
-                }
+                f.Draw();
             }
 
             //Console.ReadLine();// Ctrl + F5
+        }
+
+        static void Draw( Figure figure )
+        {
+            figure.Draw();
         }
     }
 }
